d37f7bfdbb4b3d92e4f50b4ccc1d7fcf8e675368
iryanclarke <clarke_93@hotmail.com>
2014-06-20 13:25:42 -0400

    New fonts and content updates

--- content-headline-cta.php
+++ content-headline-cta.php
@@ -2,32 +2,38 @@
   <div id="<?php echo $blocks[$counter]; ?>" class="row mainContent content-pricing headline-cta" data-block="<?php echo $blocks[$counter]; ?>">
       <div class="container">
           <div class="row">
-              <div class="small-4 columns">
+              <div class="small-2 columns">
                     <img src="img/spss_stats_icon.jpg">
               </div> 
-              <div class="small-8 columns">
+              <div class="small-10 columns">
                     <h2>IBM&reg; SPSS&reg; Statistics 22</h2>
               </div> 
           </div> 
           <div class="row">
               <div class="large-12 columns info center">
-                  <p>Use IBM SPSS Statistics to analyze data, create representative models and discover relationships between variables.</p>
+                  <h4>Use IBM SPSS Statistics to analyze data, create representative models and discover relationships between variables.</h4>
               </div> 
           </div>
           <div class="row">
-              <ul class="small-block-grid-1 medium-block-grid-1 large-block-grid-1 center">
+              <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 center">
+                  <li></li>
                   <li>
+                      <p>Students</p>
                       <a class="ctaBtn" href="http://kivuto.com/docs/student-advantage-pricing.pdf" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Lorem ipsum'); return false;">
-                          <div class="button">
-                             <span>View Pricing</span>
+                          <div class="button center">
+                             <span>BUY NOW</span>
                           </div>
                       </a>
+                  </li>    
+                  <li>    
+                      <p>Faculty/Staff</p>
                       <a class="ctaBtn" href="http://kivuto.com/docs/student-advantage-pricing.pdf" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Lorem ipsum'); return false;">
-                          <div class="button">
-                             <span>View Pricing</span>
+                          <div class="button center">
+                             <span>BUY NOW</span>
                           </div>
                       </a>
                   </li>
+                  <li></li>
               </ul>
           </div>
       </div>  
--- content-overlay-video.php
+++ content-overlay-video.php
@@ -6,12 +6,12 @@
           <div class="container">
               <div class="row">
                   <div class="large-12 columns center">
-                        <h1>IBM&reg; SPSS&reg; discounts for students & faculty</h1>
+                        <h1>IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup> discounts for students & faculty</h1>
                   </div> 
               </div> 
               <div class="row hide-for-mobile">
                   <div class="large-12 columns info center">
-                      <p>Download statistical analysis and data mining software by IBM SPSS at exlusive academic pricing</p>
+                      <p class="avenir-medium">Download statistical analysis and data mining software by IBM SPSS at exlusive academic pricing</p>
                   </div> 
               </div>
               <div class="row">
