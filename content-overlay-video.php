d69a0046cb1885c901531b4dc98978eae9dd5fbe
iryanclarke <clarke_93@hotmail.com>
2014-06-25 14:39:20 -0400

    Content changes as per angie

--- content-overlay-video.php
+++ content-overlay-video.php
@@ -6,9 +6,9 @@
                         <h1 class="avenir-heavy">IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup> discounts for students & faculty</h1>
                   </div> 
               </div> 
-              <div class="row hide-for-mobile">
+              <div class="row ">
                   <div class="large-12 columns center">
-                      <p class="avenir-medium">Download statistical analysis and data mining software by IBM SPSS at exlusive academic pricing</p>
+                      <p class="avenir-medium">Download statistical analysis and data mining software by IBM SPSS<br/> at exlusive academic pricing</p>
                   </div> 
               </div>
           </div>  
