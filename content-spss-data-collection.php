8e799955981e115340d2db3a0183d0cc50577b57
Jesse Air <jburcsik@kivuto.com>
2014-09-10 12:45:34 -0400

    fixing typo

--- content-spss-data-collection.php
+++ content-spss-data-collection.php
@@ -16,7 +16,7 @@
                       <div class="small-2 columns ">
                       </div> 
                   </div> 
-                  <p>IBM SPSS Data Collection is a dynamic research platform used to survey creation, management, and analysis.</p>
+                  <p>IBM SPSS Data Collection is a dynamic research platform used for survey creation, management, and analysis.</p>
                   <p>12 month rental licenses available to verified students and faculty.</p>
                   <div class="row ctaBtns">
                       <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-2">
