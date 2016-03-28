cbd42dde7db7aa7617f402e8c40a83d57df5ca0b
Jesse Burcsik <jburcsik@kivuto.com>
2016-03-28 14:54:00 -0400

    version 24 content and link updates
    version 24 content and link updates

--- content-headline-cta.php
+++ content-headline-cta.php
@@ -6,17 +6,17 @@
                     <img src="img/spss_stats_icon.jpg">
               </div> 
               <div class="small-10 columns">
-                    <h2>IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup><br class="special-break"/> Statistics 23</h2>
+                    <h2>IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup><br class="special-break"/> Statistics 24</h2>
               </div> 
           </div> 
           <div class="row">
               <div class="large-12 columns info center">
-                  <h4 class="avenir-light">Use IBM SPSS Statistics to analyze data, create representative models and discover relationships between variables.</h4>
+                  <h4 class="avenir-light">Use IBM SPSS Statistics to solve business and research problems by means of ad hoc analysis, hypothesis testing, geospatial analysis, and predictive analytics.</h4>
               </div> 
           </div>
           <div class="row">
               <div class="large-12 columns center">
-                  <h4 class="avenir-light">6 or 12 month rental licenses available to verified students and faculty. Available for Windows and Mac.</h4>
+                  <h4 class="avenir-light"> 6 or 12 month rental licenses available to verified students and faculty. Available for Windows and Mac.</h4>
               </div> 
           </div>
           <div class="row ctaBtns">
@@ -24,7 +24,7 @@
                   <li></li>
                   <li>
                       <h5 class="avenir-light">Students</h5>
-                      <a class="ctaBtn" href="http://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=89cf975c-47c3-e411-940a-b8ca3a5db7a1&cmi_mnuMain=ed6ad73c-7bc7-e011-ae14-f04da23e67f6&utm_source=LandingPage-SPSS-Statistics-student&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Statistics - Student Buy'); return false;">
+                      <a class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=12c7bd0a-436e-e511-9411-b8ca3a5db7a1&cmi_mnuMain=2ff73789-74c7-e011-ae14-f04da23e67f6&cmi_mnuMain_child=2a1143f0-74c7-e011-ae14-f04da23e67f6&utm_source=LandingPage-SPSS-Statistics-student&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Statistics - Student Buy'); return false;">
                           <div class="button center">
                              <span>BUY NOW</span>
                           </div>
@@ -32,7 +32,7 @@
                   </li>    
                   <li>    
                       <h5 class="avenir-light">Faculty/Staff</h5>
-                      <a class="ctaBtn" href="http://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=a89fb66a-47c3-e411-940a-b8ca3a5db7a1&cmi_mnuMain=ed6ad73c-7bc7-e011-ae14-f04da23e67f6&utm_source=LandingPage-SPSS-Statistics-faculty&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Statistics - Faculty Buy'); return false;">
+                      <a class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=6bc36423-88eb-e511-9417-b8ca3a5db7a1&cmi_mnuMain=2ff73789-74c7-e011-ae14-f04da23e67f6&cmi_mnuMain_child=2a1143f0-74c7-e011-ae14-f04da23e67f6&utm_source=LandingPage-SPSS-Statistics-faculty&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Statistics - Faculty Buy'); return false;">
                           <div class="button center">
                              <span>BUY NOW</span>
                           </div>
@@ -53,60 +53,46 @@
                       <div> 
                         <p><a class="expander" tabindex="6">Country Eligibility</a></p>
                         <div class="content">
-                            <p>Our IBM SPSS offerings are available to academic users who reside in one of the following countries:</p>
+                            <p>Our IBM SPSS offerings are available to academic users worldwide with the following exceptions</p>
                               <ul class="small-block-grid-1 medium-block-grid-2 large-block-grid-4 center">
                                   <li>
                                      <ul> 
-                                        <li><p>Antigua</p></li>
-                                        <li><p>Barbuda</p></li>
-                                        <li><p>Australia</p></li>
-                                        <li><p>Austria</p></li>
-                                        <li><p>Bahamas</p></li>
-                                        <li><p>Barbados</p></li>
-                                        <li><p>Belgium</p></li>
-                                        <li><p>Bermuda</p></li>
-                                        <li><p>Canada</p></li>
-                                        <li><p>Cyprus</p></li>
+                                        <li><p>Algeria</p></li>
+                                        <li><p>Bahrain</p></li>
+                                        <li><p>Belize</p></li>
+                                        <li><p>Costa Rica</p></li>
+                                        <li><p>Cuba</p></li>
+                                        <li><p>Dominican Republic</p></li>
+
                                      </ul>     
                                   </li>
                                   <li>
                                       <ul> 
-                                        <li><p>Denmark</p></li>
-                                        <li><p>Dominica</p></li>
-                                        <li><p>Dominican Republic</p></li>
-                                        <li><p>Estonia</p></li>
-                                        <li><p>Faroe Islands</p></li>
-                                        <li><p>Finland</p></li>
-                                        <li><p>France</p></li>
-                                        <li><p>Germany</p></li>
-                                        <li><p>Greece</p></li>
-                                        <li><p>Greenland</p></li>
+                                       <liL><p>Guatemala</p></li>
+                                       <li><p>Haiti  </p></li>
+                                       <li><p>Honduras</p></li>
+                                       <li><p>Iran, Islamic Republic Of</p></li>
+                                       <li><p>Korea, (North) Democratic People's Republic Of</p></li>
+ 
                                      </ul> 
                                   </li>    
                                   <li>    
                                       <ul> 
-                                          <li><p>Grenada</p></li>
-                                          <li><p>Ireland</p></li>
-                                          <li><p>Italy</p></li>
-                                          <li><p>Jamaica</p></li>
-                                          <li><p>Latvia</p></li>
-                                          <li><p>Luxembourg</p></li>
-                                          <li><p>Malta</p></li>
-                                          <li><p>Netherlands</p></li>
-                                          <li><p>Portugal</p></li>
-                                          <li><p>Puerto Rico</p></li>
+                                        <li><p>Kuwait</p></li>
+                                        <li><p>Nicaragua</p></li>
+                                        <li><p>Oman</p></li>
+                                        <li><p>Panama</p></li>
+                                        <li><p>Qatar</p></li>
+
                                      </ul> 
                                   </li>
                                   <li>
                                       <ul> 
-                                          <li><p>Slovakia</p></li>
-                                          <li><p>Slovenia</p></li>
-                                          <li><p>Spain</p></li>
-                                          <li><p>Sweden</p></li>
-                                          <li><p>Trinidad And Tobago</p></li>
-                                          <li><p>United Kingdom</p></li>
-                                          <li><p>United States</p></li>
-                                          <li><p>Virgin Islands, U.S.</p></li>
+                                        <li><p>Saudi Arabia</p></li>
+                                        <li><p>Sudan</p></li>
+                                        <li><p>Syrian Arab Republic</p></li>
+                                        <li><p>Tunisia</p></li>
+                                        <li><p>United Arab Emirates</p></li>
                                      </ul> 
                                   </li>
                               </ul>
--- content-spss-amos.php
+++ content-spss-amos.php
@@ -11,16 +11,16 @@
                           <img src="img/spss-amos-icon.jpg" alt="SPSS Amos">
                       </div>  
                       <div class="small-4 columns">
-                          <h2>IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup> Amos 23</h2>
+                          <h2>IBM<sup>&reg;</sup> SPSS<sup>&reg;</sup> Amos 24</h2>
                       </div> 
                       <div class="small-2 columns ">
                       </div> 
                   </div> 
                   <p>With IBM SPSS Amos, it's easy to perform structural equation modeling (SEM) to test hypotheses among variables. </p>
                   <p>12 month license available to verified students only.</p>
-                  <p class="hide-for-mobile" >Amos 22 for Windows</p>
+                  <p class="hide-for-mobile" >Amos 24 for Windows</p>
                   <div class="row ctaBtns">
-                      <a href="http://estore.onthehub.com/WebStore/OfferingDetails.aspx?o=bb3d6fb2-33c7-e411-940a-b8ca3a5db7a1&utm_source=LandingPage-SPSS-Amos&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> v2 - Clickthrough - Amos - Buy'); return false;">
+                      <a href="https://estore.onthehub.com/WebStore/OfferingDetails.aspx?o=ef357a31-afeb-e511-9417-b8ca3a5db7a1&utm_source=LandingPage-SPSS-Amos-&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> v2 - Clickthrough - Amos - Buy'); return false;">
                           <div class="large-6 columns button rent">
                              <span>BUY NOW</span>
                           </div>
