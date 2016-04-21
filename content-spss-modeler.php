62e675c634642ae97cb4897372c401c8da7e640c
unknown <dyl_hoch@live.com>
2016-04-21 15:38:40 -0400

    Update modeller page to modeller 18 section

--- content-spss-modeler.php
+++ content-spss-modeler.php
@@ -17,7 +17,8 @@
                       </div> 
                   </div> 
                   <p>IBM SPSS Modeler makes building predictive models and discovering trends in your data easy.</p>
-                  <p>12 month rental licenses available to verified students and faculty. <br>Available for Windows and Mac.</p>
+                  <p>12 month rental licenses available to verified students and faculty.</p>
+                  <p>Available for Windows and Mac.</p>
                   <div class="row ctaBtns">
                               <a class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=a662a769-6ef7-e511-9417-b8ca3a5db7a1&cmi_mnuMain=ed6ad73c-7bc7-e011-ae14-f04da23e67f6&utm_source=LandingPage-SPSS-Modeler&utm_medium=LandingPage-b&utm_campaign=SPSS" target="_blank" onclick="tob(this, '<?php echo $gaEvent; ?>', '<?php echo $gaEvent; ?> - Clickthrough', '<?php echo $gaEvent; ?> - Clickthrough -  Modeler -Student Buy'); return false;">
                                   <div class="large-6 columns button rent">
