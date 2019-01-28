39fa399f7c3d334fcf2da8b637acd0a8b66b095c
emacdougall <emacdougall@kivuto.com>
2019-01-28 10:02:22 -0500

    Add DTD 2019 Banner

--- dtd-footer.php
+++ dtd-footer.php
@@ -1,21 +1,20 @@
-<div class="special-footer dtd-promo" id="CollegeGear">
-	<a href="https://onthehub.com/drop-the-debt/?utm_source=dtd-footer&utm_medium=spss-lp&utm_campaign=onthehub#contest" target="_blank">
-		<img class="money" src="https://onthehub.com/wp-content/themes/onthehub/images/dtd/money.png" />
-		<p>Don’t let debt stand in your way! <span class="cgg-cta">Enter Drop The Debt</span> for your chance to win $250 this week or the $1000 grand prize!</p>
+<div class="special-footer dtd-promo" id="DTD">
+	<a href="https://onthehub.com/drop-the-debt/?utm_source=dtd-footer&utm_medium=spss-lp&utm_campaign=DTD#Enter" target="_blank">
+		<img class="money" src="https://onthehub.com/wp-content/uploads/2019/01/sticky-money.png" />
+		<p>Enter the Drop the Debt Giveaway this week for your chance to win $250 USD or the $1000 Grand Prize! <span class="dtd-cta">Enter Now</span></p>
 	</a>
 </div>
 
 <!-- For The College Gear Giveaway Special Footer -->
 <style>
-	@import url('https://fonts.googleapis.com/css?family=Barlow+Condensed:500i');
-	footer {
-		margin-bottom: 78px;
+	footer.row {
+		margin-bottom: 88px;
 	}
 	#at4m-mobile-container {
 		display: none !important; 
 	}
 	div.special-footer.dtd-promo {
-		background: #0B2C3B;
+		background: #1A535D;
 		position: fixed;
 		bottom: 0;
 		z-index: 100000;
@@ -24,67 +23,82 @@
 		color: #fff;
 		width: 100%;
 		display: table;
+		box-shadow: 0 -2px 5px rgba(0,0,0,0.2);
+		transition: 750ms;
+		margin-bottom: -100px;
 	}
-	div.special-footer.dtd-promo .cgg-cta {
-		border-bottom: 1px solid #D6DCA0;
-		color: #D6DCA0;
-		font-weight: 500;
-		font-style: italic;
-		font-family: 'Barlow Condensed', sans-serif !important;
+	div.special-footer.dtd-promo.visible {
+		margin-bottom: 0;
+	}
+	div.special-footer.dtd-promo span {
+		font-weight: 700;
+		font-family: inherit !important;
+	}
+	div.special-footer.dtd-promo .dtd-cta {
+		white-space: nowrap;
+		color: #D0F2BE;
+		text-decoration: underline;
+	}
+	div.special-footer.dtd-promo .dtd-cta:hover {
+		color: #f5fff0;
 	}
 	div.special-footer.dtd-promo p {
-		font-family: 'Barlow Condensed', sans-serif !important;
-    	font-weight: 500;
-		font-style: italic;
-		font-size: 22px;
-		padding: 5px 10px;
-		display: block;
+		font-family: 'Open Sans', sans-serif !important;
+		color: #fff;
+    	font-weight: 700;
+		font-size: 18px;
+		padding: 8px 40px 5px 40px;
+		display: table-cell;
 		vertical-align: middle;
 		text-align: left;
-		line-height: 1.2em;
-		margin: 10px 0 5px 260px;
+		line-height: 1.4em;
+		margin: 0;
 	}
 	div.special-footer.dtd-promo a {
-		font-family: 'Barlow Condensed', sans-serif !important;
-    	font-weight: 500;
-		font-style: italic;
+		font-family: 'Open Sans', sans-serif !important;
+    	font-weight: inherit;
 		color:#fff;
 		display: table;
 		text-decoration: none;
-		max-width: 767px;
+		max-width: 900px;
+		height: 100%;
 		margin: 0 auto;
 	}
 	div.special-footer.dtd-promo img.money {
-		display: block;
-		position: absolute;
-		width: 230px;
-		max-width: 230px;
+		display: table-cell;
+		width: 144px;
+		max-width: 144px;
 		height: auto;
-		bottom: -20px;
+		margin: 10px 0 8px 20px;
 		transition: 250ms;
 	}
 	div.special-footer.dtd-promo a:hover img.money {
-		bottom: 0;
+		transform: rotate(4deg);
 	}
-	@media (max-width: 680px) {
-		div.special-footer.dtd-promo a:hover img.money, div.special-footer.dtd-promo img.money  {
-			bottom: -7px;
-			left: -90px;
-		}
+	@media (max-width: 920px) {
 		div.special-footer.dtd-promo p {
-			font-size: 18px;
-			margin: 10px 0 5px 160px;
-			margin-bottom: 5px !important;
+			font-size: 16px;
 		}
 	}
-	@media (max-width: 480px) {
+	@media (max-width: 767px) {
+		div.special-footer.dtd-promo a img.money {
+			margin-left: -80px;
+		}
+	}
+	@media (max-width: 580px) {
 		div.special-footer.dtd-promo a:hover img.money, div.special-footer.dtd-promo img.money  {
 			display: none;
 		}
 		div.special-footer.dtd-promo p {
 			text-align: center;
-			margin: 10px 15px 10px 15px;
-			margin-bottom: 10px !important;
+			font-size: 15px;
 		}
 	}
 </style>
+<script>
+	$(document).ready(function(){
+		setTimeout(function(){
+			$("div.special-footer.dtd-promo").addClass("visible");
+		},1000);
+	});
+</script>
