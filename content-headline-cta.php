324b15f0005d542736dc0937d1079f26cdf9bfd5
Son Tran <sontran@MacBook-Air-cua-Son.local>
2020-06-17 18:22:24 -0400

    Update Spss Landing Page 27

--- content-headline-cta.php
+++ content-headline-cta.php
@@ -3,29 +3,29 @@
       <div class="container">
           <div class="row">
               <div class="large-5 columns qcImage hide-for-large-up">
-                  <img src="img/stats-screens.png" alt="SPSS Statistics 26 on a Laptop with a Chart and Graph" style="max-height: 300px; width:auto;">
+                  <img src="img/lp-stats-screens-27.png" alt="SPSS Statistics 27 on a Laptop with a Chart and Graph" style="max-height: 300px; width:auto;">
               </div>
               <div class="large-7 columns info">
                   <div class="row title">
-                    <h2>IBM<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> SPSS<sup style="font-size:0.5em;"><sup>&reg;</sup></sup><br class="special-break"/> Statistics 26</h2>
+                    <h2>IBM<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> SPSS<sup style="font-size:0.5em;"><sup>&reg;</sup></sup><br class="special-break"/> Statistics 27</h2>
                 </div>
                 <div class="padding-for-mobile">  
                 	<p>Use IBM SPSS Statistics to solve business and research problems by means of ad hoc analysis, hypothesis testing, geospatial analysis, and predictive analytics. 6- or 12-month rental licenses available to verified students and faculty. Available for Windows and Mac.</p>
-					<p><strong class="red">$39.95 USD</strong> – Students <strong>Save 97%</strong></p>
-					<p><strong class="red">$259.99 USD</strong> – Faculty <strong>Save 78%</strong></p>
+					<p><strong class="blue">$39.95 USD</strong> – Students <strong>Save 97%</strong></p>
+					<p><strong class="blue">$259.99 USD</strong> – Faculty <strong>Save 78%</strong></p>
                 </div>
               <br/>
               <div class="ctaBtns center-small">
                   <ul class="small-block-grid-1 medium-block-grid-4 large-block-grid-4 center">
                       <li>
-                          <a data-role="button" data-cta="Buy SPSS for Students" class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=304b1dff-5b5d-e911-8115-000d3af41938" target="_blank">
+                          <a data-role="button" data-cta="Buy SPSS for Students" class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=0a915582-02b0-ea11-812c-000d3af41938" target="_blank">
                               <div class="button center">
                                  <span><em>Students</em> buy now</span>
                               </div>
                           </a>
                       </li>    
                       <li>    
-                          <a data-role="button" data-cta="Buy SPSS for Faculty" class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=2d4b1dff-5b5d-e911-8115-000d3af41938" target="_blank">
+                          <a data-role="button" data-cta="Buy SPSS for Faculty" class="ctaBtn" href="https://estore.onthehub.com/WebStore/OfferingDetails.aspx?o=ef512568-aab0-ea11-812c-000d3af41938" target="_blank">
                               <div class="button center">
                                   <span><em>Faculty</em> buy now</span>
                               </div>
@@ -36,7 +36,7 @@
               </div>
           </div>
            <div class="large-5 columns show-for-large-up">
-                  <img src="img/stats-screens.png" alt="IBM SPSS Statistics 26 on a Laptop with a Chart and Graph" style="max-height: 349px; width:474px;">
+                  <img src="img/lp-stats-screens-27.png" alt="SPSS Statistics 27 on a Laptop with a Chart and Graph" style="max-height: 349px; width:474px;">
               </div>
           </div> 
       </div>  
--- content-spss-prev-versions.php
+++ content-spss-prev-versions.php
@@ -3,17 +3,17 @@
       <div class="container">
           <div class="row">
                <div class="large-5 columns grid-icons">
-				   <a data-role="button" data-cta="SPSS Statitics 25" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=fc2dece7-5e83-e711-80f3-000d3af41938" target="_blank" class='main-prev'><img src="img/spss-25-boxshot.jpg" alt="SPSS Statistics 25 GradPack" /></a>
+				   <a data-role="button" data-cta="SPSS Statitics 26" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=304b1dff-5b5d-e911-8115-000d3af41938" target="_blank" class='main-prev'><img src="img/previous-26-boxshot.jpg" alt="SPSS Statistics 26 GradPack" /></a>
 				   <div class="sub-prev">
+					   <a data-role="button" data-cta="SPSS Statitics 25" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=fc2dece7-5e83-e711-80f3-000d3af41938" target="_blank"><img src="IMG/previous-25-boxshot.jpg" alt="SPSS Statistics 25"></a>
 					   <a data-role="button" data-cta="SPSS Statitics 24" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=12c7bd0a-436e-e511-9411-b8ca3a5db7a1" target="_blank"><img src="img/spss-24-boxshot.jpg" alt="SPSS Statistics 24"></a>
-					   <a data-role="button" data-cta="SPSS Statitics 23" href="https://estore.onthehub.com/WebStore/OfferingsOfMajorVersionList.aspx?pmv=89cf975c-47c3-e411-940a-b8ca3a5db7a1" target="_blank"><img src="img/spss-23-boxshot.jpg" alt="SPSS Statistics 23"></a>
 				   </div>
               </div>
               <div class="large-6 columns info">
                   <div class="row title"> 
                         <h2>Looking for previous versions?</h2>
                   </div> 
-                  	<p>You can still order <strong>IBM<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> SPSS<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> Statistics 23, 24,</strong> and <strong>25</strong> through OnTheHub. Students and faculty can both take comfort in knowing that the software they use matches course materials.</p>
+                  	<p>You can still order <strong>IBM<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> SPSS<sup style="font-size:0.5em;"><sup>&reg;</sup></sup> Statistics 24, 25,</strong> and <strong>26</strong> through OnTheHub. Students and faculty can both take comfort in knowing that the software they use matches course materials.</p>
 				  	<p><strong class="red">$42.99 USD</strong> – Students <strong>Save 96%</strong></p>
                   <br />
                   <div class="row ctaBtns">
