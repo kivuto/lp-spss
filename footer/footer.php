d2960110f081b6785b5b29b577cb069f77fb4a68
emacdougall <emacdougall@kivuto.com>
2020-11-18 14:28:53 -0500

    SPSS - Reverting Newsletter Form to MailChimp

--- footer/footer.php
+++ footer/footer.php
@@ -30,12 +30,35 @@
 		</div>
 		<div class="large-4 columns newsletter">
 			<p class="big-text">Newsletter Sign Up</p>
-			<script>
-					  hbspt.forms.create({
-						portalId: "4594424",
-						formId: "534f6ee8-3562-4c83-bad5-91801c75a3df"
-					});
-			</script>
+			<!-- Begin MailChimp Signup Form -->
+			<div id="mc_embed_signup">
+			<form action="https://onthehub.us20.list-manage.com/subscribe/post?u=7920b28730d6d155b035115d0&amp;id=1c081a8bba" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+				<div id="mc_embed_signup_scroll">
+					<div class="mc-field-group">
+						<label for="mce-EMAIL" class="hidden" id="email_label">Email address</label>
+						<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" aria-labelledby='email_label' placeholder="Email address">
+					</div>
+					<div class="mc-field-group field-right">
+						<label for="mce-USERGROUP" class="hidden" id="ugroup_label">Are you a student or faculty member?</label>
+						<select name='USERGROUP' id='USERGROUP' aria-labelledby='ugroup_label'>
+							<option value='Students' selected>I'm a student</option>
+							<option value='Faculty'>I'm a faculty member</option>
+							<option value=''>Other</option>
+						</select>
+					</div>
+					<div style="position: absolute; left: -5000px;" aria-hidden="true">
+						<input type="text" value="<?php echo $gaEvent; ?>" name="SIGNUP" class="" id="mce-SIGNUP">
+					</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->		
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_56207817284eac40f14c245a7_2deb975deb" tabindex="-1" value=""></div>
+					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+				</div>
+				</form>
+			</div>
+			<!--End mc_embed_signup-->
 		</div>
 	</div>  
 </div> 
--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <link rel="stylesheet" href="css/main.css" />
     <link rel="stylesheet" href="css/jquery.fancybox.css?v=2.1.5"/>
     <link rel="stylesheet" href="css/font-awesome.min.css">
+	<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
     <script src="js/vendor/modernizr.js"></script>
     <script src="js/vendor/jquery.js"></script>
     <script src="js/viewport.js"></script>
     <script src="js/jquery.form.js"></script>        
     <script type="text/javascript" src="js/jquery.fancybox.pack.js?v=2.1.5"></script>
-	<!--[if lte IE 8]><script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2-legacy.js"></script><![endif]-->
-	<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/v2.js"></script>
     <?php 
         /*-=-=-=-=-=-=-=-=-=- CUSOMIZE YOUR ACCOUNTS/VARIABLES AS NEEDED =-=-=-=-=-=-=-=*/
 
@@ -96,7 +95,8 @@
 
       </div>
   </div>
-    
+	  
+    <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='USERGROUP';ftypes[1]='text';fnames[2]='SIGNUP';ftypes[2]='text';fnames[3]='FNAME';ftypes[3]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
     <script src="js/waypoints.min.js"></script>
     <script src="js/waypoints.min.js"></script>
     <script src="js/simple-expand.min.js"></script>
